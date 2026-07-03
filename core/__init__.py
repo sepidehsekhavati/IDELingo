6def7ec0a3a1e6bc43a996f52b912c196b85813d
sepidehsekhavati <60705783+sepidehsekhavati@users.noreply.github.com>
2026-07-03 07:37:41 +0330

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -4,6 +4,7 @@ from .GrammarEnhancer import GrammarEnhancer
 from .OfflineDictionary import OfflineDictionary  # اضافه شد
 from .AdvancedGrammarChecker import AdvancedGrammarChecker
 from .PlanManager import PlanManager
+from .CloudSync import CloudSync
 
-__all__ = ["UserManager", "Database", "GrammarEnhancer", 
-           "OfflineDictionary", "AdvancedGrammarChecker", "PlanManager"]
+__all__ = ["UserManager", "Database", "GrammarEnhancer",
+           "OfflineDictionary", "AdvancedGrammarChecker", "PlanManager", "CloudSync"]
